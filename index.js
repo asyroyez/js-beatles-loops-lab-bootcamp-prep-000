387e1aa589765dff0f00bfe7c069c89083f25da0
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-10-24 18:56:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments) {
+  const beatles = []
+  for (let i = 0; i < 4; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles
+}
+
+function johnLennonFacts(facts) {
+  while (let i = 0; i > 4; i++) {
+    text = facts[i] + "!!!"
+  }
+  return text
+}
